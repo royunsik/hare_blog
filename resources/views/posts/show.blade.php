60f66b6d475935ec1c1dbb172f513327525341ee
royunsik <yunsikro@gmail.com>
2023-09-20 16:43:45 +0900

    リファクタリング

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,6 +6,8 @@
                 {{ session('notice') }}
             </div>
         @endif
+        <x-validation-errors :errors="$errors"/>
+
         @if ($errors->any())
             <div class="bg-red-100 border-l-4 border-red-500 text-red-700 p-4 my-2" role="alert">
                 <p>
